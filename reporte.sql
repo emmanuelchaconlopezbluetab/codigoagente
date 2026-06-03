32f48689fd190357f1091309e6060bbcbfbb5aac
Emmanuel Chacon Lopez <emmanuel.chacon@bluetab.net>
2026-06-03 22:29:29 +0000

    ajusets

--- reporte.sql
+++ reporte.sql
@@ -1,5 +1,6 @@
 
 CREATE OR REPLACE TABLE `bluetab-colombia-data-qa.bluetabh.perfiles_git`
 AS
+---- Datos nuevos
 SELECT * FROM `bluetab-colombia-data-qa.bluetabh.perfiles`
 WHERE Nombre IS NOT NULL
